7f5fbab825d44c02c7d0b62fcbc2fddefe2de627
Scorpion180 <43318493+Scorpion180@users.noreply.github.com>
2019-05-19 21:01:37 -0500

    agrega menú

--- Interprete/luz.py
+++ Interprete/luz.py
@@ -1,3 +1,18 @@
-
+import constantes
 def luz():
+    print('-------------------------')
+    print('1.- Emitir')
+    print('2.- Recibir')
+    print('-------------------------')
+    try:
+        opcion = int(raw_input())
+        if (opcion == 1):
+            cadena = raw_input("Ingresa la cadena: ")
+            constantes.enviaCadArduinoLuz(cadena)
+
+        elif (opcion == 2):
+    # Pendiente leer luz
+    except ValueError:
+        print('Valor no valido')
+
     pass
